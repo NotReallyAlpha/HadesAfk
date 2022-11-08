31d4a0fa8f47be7d864dd7a762098577349d241c
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-08 21:48:45 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+from os import environ as e
+
+class API:
+    API_ID = e["API_ID"]
+    API_HASH = e["API_HASH"]
+
+class TOKEN:
+    BOT_TOKEN = e["BOT_TOKEN"]
+
+class DB:
+    MONGO_DB_URL = e["MONGO_DB_URL"]
+
+class DEV:
+    SUDO_USERS_STR = e["SUDO_USERS"].split()
+    SUDO_USERS = []
+    for x in SUDO_USERS_STR:
+        SUDO_USERS.append(int(x))
