929888b9e6498b973f6ccd10eb519857fffbb8f7
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-08 21:56:43 +0530

    Update yashu.py

--- yashu.py
+++ yashu.py
@@ -1,3 +1,6 @@
 from config import API, TOKENS
 from pyrogram import Client as Hades, idle
-from pyrogram.filters import command as 
+from pyrogram.filters import command as hade_cmd
+
+hades = Hades(":Hades:", api_id=API.API_ID, api_hash=API.API_HASH, bot_token=TOKENS.BOT_TOKEN)
+
