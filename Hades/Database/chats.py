2e62d4b70c3bcff8623640d4c39bebd4570bffdc
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-08 22:05:12 +0530

    Update chats.py

--- Hades/Database/chats.py
+++ Hades/Database/chats.py
@@ -1,1 +1,18 @@
 from . import db
+
+chatsdb = db.chats
+
+async def add_chat(chat_id: int):
+    x = await chatsdb.find_one({"chat_id": chat_id})
+    if not x:
+        return await chatsdb.insert_one({"chat_id": chat_id})
+    return
+
+async def get_chats():
+    x = chatsdb.find({"chat_id": {"$gt": 0}})
+    if not x:
+        return []
+    CHATS = []
+    for y in await x.to_list(length=1000000000):
+        CHATS.append(int(y))
+    return CHATS
