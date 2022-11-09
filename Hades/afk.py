da6dc934bcec92d1690ae2f21096bf2ebf4644c1
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-09 12:15:14 +0530

    Update afk.py

--- Hades/afk.py
+++ Hades/afk.py
@@ -10,12 +10,47 @@ async def afk(_, m):
             if reply.photo:
                 await _.download_media(reply, file_name=f"{user_id}.jpg")
                 _reason = m.text.split(None, 1)[1] if len(m.command) > 1 else None
-                _time_afk = time.time()
+                time_afk = time.time()
                 details = {
                           "type": "photo",
-                          "reason" : _reason,
-                          "time" : time_afk
+                          "reason": _reason if _reason else None,
+                          "time": time_afk
                           }
-        
+            elif reply.sticker:
+                if reply.sticker.is_animated:
+                    _reason = m.text.split(None, 1)[1]
+                    time_afk = time.time()
+                    details = {
+                          "type": "text",
+                          "reason": _reason if _reason else None,
+                          "time": time_afk
+                          }
+                else:
+                    await _.download_media(reply, file_name=f"{user_id}.jpg")
+                    _reason = m.text.split(None, 1)[1] if len(m.command) > 1 else None
+                    time_afk = time.time()
+                    details = {
+                          "type": "photo",
+                          "reason": _reason if _reason else None,
+                          "time": time_afk
+                          }
+            elif reply.animation:
+                _data = reply.animation.file_id
+                _reason = m.text.split(None, 1)[1]
+                time_afk = time.time()
+                details = {
+                          "type": "animation",
+                          "reason": _reason if _reason else None,
+                          "time": time_afk,
+                          "data": _data
+                          }
+            else:
+                _reason = m.text.split(None, 1)[1]
+                time_afk = time.time()
+                details = {
+                          "type": "text",
+                          "reason": _reason if _reason else None,
+                          "time": time_afk
+                          }
                  
         
