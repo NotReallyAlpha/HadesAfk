519c2ced381b720b6f467f5ad9d8a54f0067ca7e
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-09 18:20:06 +0530

    Update yashu.py

--- yashu.py
+++ yashu.py
@@ -4,20 +4,43 @@ from pyrogram.filters import command as hade_cmd, new_chat_members
 from Hades.afk import afk
 from Hades.watcher import afk_reply_watcher, afk_watcher, welcome
 
+txt = "modules imported !"
+
 hades = Hades(":Hades:", api_id=API.API_ID, api_hash=API.API_HASH, bot_token=TOKENS.BOT_TOKEN)
+
+txt += "\nClient verified !"
 
 @hades.on_message(hade_cmd(["afk"]) | hade_cmd("brb", ""))
 async def afk_plug(_, m):
     await afk(_, m)
 
+txt += "\nMain module loaded !"
+
 @hades.on_message(group=1)
 async def watcher1(_, m):
     await afk_watcher(_, m)
+
+txt += "\nWatcher 1 loaded !"
 
 @hades.on_message(group=2)
 async def watcher2(_, m):
     await afk_reply_watcher(_, m)
 
+txt += "\nWatcher 2 loaded !"
+
 @hades.on_message(group=3 & new_chat_members)
 async def welcome_plug(_, m):
     await welcome(_, m)
+
+txt += "\nWatcher 3 loaded !"
+
+def Asynchorous(x, t):
+    x.start()
+    y = x.get_me()
+    z = y.username
+    t += f"\n@{z} Started Successfully !"
+    print(t)
+    idle()
+
+Asynchorous(hades, txt)
+
