732d5dad1667ed87d185f50fb46d77ffffcd8d3c
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-09 11:55:41 +0530

    Update afk.py

--- Hades/afk.py
+++ Hades/afk.py
@@ -9,7 +9,13 @@ async def afk(_, m):
         if reply:
             if reply.photo:
                 await _.download_media(reply, file_name=f"{user_id}.jpg")
-                reason = m.text.split(None, 1)[1] if len(m.command) > 1 else None
-                time_afk = time.time()
+                _reason = m.text.split(None, 1)[1] if len(m.command) > 1 else None
+                _time_afk = time.time()
+                details = {
+                          "type": "photo",
+                          "reason" : _reason,
+                          "time" : time_afk
+                          }
+        
                  
         
