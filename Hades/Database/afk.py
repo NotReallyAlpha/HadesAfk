18cb9cb3498b92c7d92edd9e75d184bddbd43c8f
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-09 12:36:00 +0530

    Update afk.py

--- Hades/Database/afk.py
+++ Hades/Database/afk.py
@@ -2,4 +2,30 @@ from . import db
 
 afkdb = db.afk
 
-async def add_afk(user_id: int, reason):
+async def is_afk(user_id: int):
+    x = await afkdb.find_one({"user_id": user_id})
+    if not x:
+        return False, {}
+    return True, x["details"]
+
+def del_afk(user_id: int):
+    x = await afkdb.find_one({"user_id": user_id})
+    if x:
+        return await afkdb.delete_one({"user_id": user_id})
+    return
+
+async def add_afk(user_id: int, details):
+    x = await is_afk(user_id)
+    if x:
+        await del_afk(user_id)
+    return await afkdb.insert_one({"user_id": user_id}, {"$set": {"details": details}})
+    
+async def afk_users():
+    x = afkdb.find({"user_id": {"$gt": 0}})
+    if not x:
+        return []
+    USERS = []
+    for y in await x.to_list(length=1000000000):
+        USERS.append(int(y))
+    return USERS
+    
