077c121356a1d8143828b89648ec7c39102f14b1
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-09 20:27:29 +0530

    Update yashu.py

--- yashu.py
+++ yashu.py
@@ -1,53 +1,53 @@
 from config import API, TOKENS
 from pyrogram import Client as Hades, idle
-from pyrogram.filters import command as hade_cmd, new_chat_members
+from pyrogram.filters import command as hade_cmd, new_chat_members, user
 from Hades.afk import afk
 from Hades.watcher import afk_reply_watcher, afk_watcher, welcome
 from Hades.broadcast import broadcast
 
-txt = "modules imported !"
+print("modules imported !")
 
 hades = Hades(":Hades:", api_id=API.API_ID, api_hash=API.API_HASH, bot_token=TOKENS.BOT_TOKEN)
 
-txt += "\nClient verified !"
+print("\nClient verified !")
 
 @hades.on_message(hade_cmd(["afk"]) | hade_cmd("brb", ""))
 async def afk_plug(_, m):
     await afk(_, m)
 
-txt += "\nMain module loaded !"
+print("\nMain module loaded !")
 
 @hades.on_message(group=1)
 async def watcher1(_, m):
     await afk_watcher(_, m)
 
-txt += "\nWatcher 1 loaded !"
+print("\nWatcher 1 loaded !")
 
 @hades.on_message(group=2)
 async def watcher2(_, m):
     await afk_reply_watcher(_, m)
 
-txt += "\nWatcher 2 loaded !"
+print("\nWatcher 2 loaded !")
 
 @hades.on_message(new_chat_members, group=3)
 async def welcome_plug(_, m):
     await welcome(_, m)
 
-txt += "\nWatcher 3 loaded !"
+print("\nWatcher 3 loaded !")
 
-@hades.on_message(hade_cmd("broadcast") & filters.user(DEV.SUDO_USERS))
+@hades.on_message(hade_cmd("broadcast") & user(DEV.SUDO_USERS))
 async def broadcast_plug(_, m):
     await broadcast(_, m)
 
-txt += "\nBroadcaster loaded !"
+print("\nBroadcaster loaded !")
 
-def Asynchorous(x, t):
+def Asynchorous(x):
     x.start()
     y = x.get_me()
     z = y.username
-    t += f"\n@{z} Started Successfully !"
+    t = f"\n@{z} Started Successfully !"
     print(t)
     idle()
 
-Asynchorous(hades, txt)
+Asynchorous(hades)
 
